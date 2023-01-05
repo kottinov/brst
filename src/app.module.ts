f5c2c8ead3243a0bd07f0ee9697d4cd249834128
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2023-01-05 22:45:33 +0200

    Removed sockets for now

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,9 @@ import { DronesService } from './drones/drones.service';
 import { DronesController } from './drones/drones.controller';
 import { PilotsService } from './pilots/pilots.service';
 import { PilotsController } from './pilots/pilots.controller';
-import { SocketsModule } from './sockets/sockets.module';
 
 @Module({
-  imports: [SocketsModule],
+  imports: [],
   controllers: [AppController, DronesController, PilotsController],
   providers: [AppService, DronesService, PilotsService],
 })
