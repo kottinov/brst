1fdd9524f4e7b54a96e96bfde85b037bcaae1be2
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2023-01-05 15:46:31 +0200

    Set up a request through pilots service

--- src/pilots/pilots.service.ts
+++ src/pilots/pilots.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class PilotsService {}
+export class PilotsService {
+  async getPilot(serialNumber: string): Promise<any> {
+    const response = await axios.get(
+      `https://assignments.reaktor.com/birdnest/pilots/${serialNumber}`,
+    );
+    if (response.status === 404) {
+      throw new NotFoundException(
+        `Pilot not found for serial number ${serialNumber}`,
+      );
+    }
+    return response.data;
+  }
+}
