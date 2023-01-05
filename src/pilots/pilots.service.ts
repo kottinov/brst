3d2f97cd3da42ce8d42ad095d127e6120873eff1
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2023-01-05 22:45:03 +0200

    Wrapped getPilot in tryCatch

--- src/pilots/pilots.service.ts
+++ src/pilots/pilots.service.ts
@@ -4,14 +4,16 @@ import axios from 'axios';
 @Injectable()
 export class PilotsService {
   async getPilot(serialNumber: string): Promise<any> {
-    const response = await axios.get(
-      `https://assignments.reaktor.com/birdnest/pilots/${serialNumber}`,
-    );
-    if (response.status === 404) {
-      throw new NotFoundException(
-        `Pilot not found for serial number ${serialNumber}`,
+    try {
+      const response = await axios.get(
+        `https://assignments.reaktor.com/birdnest/pilots/${serialNumber}`,
       );
+      return response.data;
+    } catch (error) {
+      if (error.response.status === 404) {
+        throw new NotFoundException('Pilot not found');
+      }
+      throw error;
     }
-    return response.data;
   }
 }
