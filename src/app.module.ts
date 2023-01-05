b67d2eb8031a661e4f24654784afa1980dad7cc1
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2023-01-05 14:54:59 +0200

    Generated drones service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DronesService } from './drones/drones.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DronesService],
 })
 export class AppModule {}
