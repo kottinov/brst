07364479ed7e6e96f6c57a647d760ec6e3409509
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2023-01-05 15:24:21 +0200

    Generated pilots service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DronesService } from './drones/drones.service';
 import { DronesController } from './drones/drones.controller';
+import { PilotsService } from './pilots/pilots.service';
 
 @Module({
   imports: [],
   controllers: [AppController, DronesController],
-  providers: [AppService, DronesService],
+  providers: [AppService, DronesService, PilotsService],
 })
 export class AppModule {}
