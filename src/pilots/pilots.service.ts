18386a0038853f367378068fecf64a26e329bf75
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2023-01-06 19:01:29 +0200

    Installed NodeCache, made pilot information to persist for 10 mins

--- src/pilots/pilots.service.ts
+++ src/pilots/pilots.service.ts
@@ -1,19 +1,30 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import axios from 'axios';
-
+import * as NodeCache from 'node-cache';
 @Injectable()
 export class PilotsService {
+  cache = new NodeCache({
+    stdTTL: 600, // 10 minutes
+    checkperiod: 120, // 2 minutes
+  });
+
   async getPilot(serialNumber: string): Promise<any> {
-    try {
-      const response = await axios.get(
-        `https://assignments.reaktor.com/birdnest/pilots/${serialNumber}`,
-      );
-      return response.data;
-    } catch (error) {
-      if (error.response.status === 404) {
-        throw new NotFoundException('Pilot not found');
+    const pilot = this.cache.get(serialNumber);
+    if (!pilot) {
+      try {
+        const response = await axios.get(
+          `https://assignments.reaktor.com/birdnest/pilots/${serialNumber}`,
+        );
+        const pilot = response.data;
+        this.cache.set(serialNumber, pilot);
+        return pilot; // because otherwise it would return undefined if the pilot was not in the cache
+      } catch (error) {
+        if (error.response.status === 404) {
+          throw new NotFoundException('Pilot not found');
+        }
+        throw error;
       }
-      throw error;
     }
+    return pilot;
   }
 }
