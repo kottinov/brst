85e6303dc88817e0fc4b15c4a2da36ed610f4698
Alexandr Constantinov <76010869+o-tidev@users.noreply.github.com>
2023-01-09 10:24:38 +0200

    Enabled CORS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DronesService } from './drones/drones.service';
 import { DronesController } from './drones/drones.controller';
 import { PilotsService } from './pilots/pilots.service';
 import { PilotsController } from './pilots/pilots.controller';
+import { CorsOptions } from 'cors';
+import * as cors from 'cors';
+
+const corsOptions: CorsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
 @Module({
   imports: [],
   controllers: [AppController, DronesController, PilotsController],
   providers: [AppService, DronesService, PilotsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cors(corsOptions)).forRoutes('*');
+  }
+}
